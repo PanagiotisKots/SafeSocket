ace9f1ae094e5566d163498ad427d45a32348349
panagiotis kotsorgios <161494302+PanagiotisKots@users.noreply.github.com>
2024-06-18 23:02:01 +0300

    Update homepage.cpp

--- V - 0.1/server/homepage.cpp
+++ V - 0.1/server/homepage.cpp
@@ -46,7 +46,7 @@ void homepage(){
 	cout<<"***********************************";Set_Console_Color(FOREGROUND_RED, 0);cout<<"*******************************"<<endl;
 	cout<<"=> Developed by: [Kotsorgios Panagiotis].\n";
 	cout<<"=> Utilizes: [AES] Advanced Encryption Protocol.";Set_Console_Color(FOREGROUND_RED, 0);cout<<"   (!) ";Set_Console_Color(FOREGROUND_YELLOW, 0);cout<<"not yet available\n";Set_Console_Color(FOREGROUND_RED, 0);
-    cout<<"\n=> Windows system compatible.\n";
+        cout<<"\n=> Windows system compatible.\n";
 	cout<<"=> Fully written in C++ programming language."<<endl;
 	cout<<"=> Active Contributors: [Kotsorgios Panagiotis] && [Gkontai Aldo]."<<endl<<endl<<endl;
 	
